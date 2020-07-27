9ee7373187f363b0e0d60752ce5a9097b4446b0a
pankaj marghade <p.g.marghade@gmail.com>
2020-07-27 16:59:49 +0530

    added comment about RapidAPI key

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,6 +3,8 @@ const apiConstants = require("../constants/api")
 
 axios.defaults.headers.common["x-rapidapi-host"] = apiConstants.RAPIDAPI_HOST;
 axios.defaults.headers.common["x-rapidapi-key"] = apiConstants.RAPIDAPI_KEY;
+// This key should read from environment variable in production 
+
 axios.defaults.headers.common["useQueryString"] = true;
 
 module.exports = {
